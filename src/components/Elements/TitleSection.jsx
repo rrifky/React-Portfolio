34d97a99284f47d25b49ba1a81286c99faf8a822
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-03-07 19:49:31 +0700

    tambahan dikit

--- src/components/Elements/TitleSection.jsx
+++ src/components/Elements/TitleSection.jsx
@@ -5,7 +5,7 @@ const TitleSection = ({ title, subtitle }) => {
   return (
     <div className="mt-8">
       <h1 className={`text-xl md:text-3xl font-medium mb-1 ${isDarkMode ? 'text-slate-50' : 'text-slate-800'}`}>{title}</h1>
-      <h3 className={`text-base md:text-xl text-slate-700 ${isDarkMode ? 'text-slate-200' : 'text-slate-700'}`}>{subtitle}</h3>
+      <h3 className={`text-base md:text-xl ${isDarkMode ? 'text-slate-200' : 'text-slate-700'}`}>{subtitle}</h3>
     </div>
   );
 };
