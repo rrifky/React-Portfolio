0cdef8e1b3b560f79b647c9f12201907a04aa0c0
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-03-10 23:00:54 +0700

    i want to sleep

--- src/Pages/contactPages.jsx
+++ src/Pages/contactPages.jsx
@@ -1,6 +1,5 @@
 import ContactForm from '../components/Elements/ContactForm';
 import Sosmed from '../components/Elements/Sosmed';
-import TitleSection from '../components/Elements/TitleSection';
 import { FaLinkedin, FaGithub, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useTheme } from '../context/ThemeContext';
@@ -11,7 +10,7 @@ const ContactPages = () => {
   return (
     <div className={`h-screen ${isDarkMode ? 'bg-zinc-900' : 'bg-white'} `}>
       <div className={`mx-auto max-w-[340px] mb-8 md:max-w-[720px] lg:max-w-[920px]`}>
-        <motion.div className="mt-16 xl:mt-0 xl:ml-8" initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8 }}>
+        <motion.div className="pt-16 xl:mt-0 xl:ml-8" initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8 }}>
           <div className={`text-xl md:text-3xl font-medium pt-5 ${isDarkMode ? 'text-slate-50' : 'text-slate-800'}`}>Contact</div>
           <div className={`border-t border-slate-300 pt-5 mt-2 md:text-lg  ${isDarkMode ? 'text-slate-200' : 'text-slate-700'}`}>Let's Connect</div>
           <div className="flex flex-wrap my-6">
